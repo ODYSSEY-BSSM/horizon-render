4801e9cce7452666d9a7aa144082306187dae7dc
sunz8281 <sunz8281@gmail.com>
2025-08-16 18:32:38 +0900

    feat(#06): FlexBox 컴포넌트 제작

---
+++ packages/ui/src/components/flexBox/FlexBox.tsx
@@ -0,0 +1,48 @@
+import { clsx } from "clsx";
+import type React from "react";
+
+interface FlexBoxProps extends React.HTMLAttributes<HTMLDivElement> {
+  direction?: React.CSSProperties["flexDirection"];
+  justify?: React.CSSProperties["justifyContent"];
+  align?: React.CSSProperties["alignItems"];
+  wrap?: React.CSSProperties["flexWrap"];
+  gap?: string | number;
+  grow?: number;
+  shrink?: number;
+  basis?: React.CSSProperties["flexBasis"];
+  inline?: boolean;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const FlexBox = ({
+  direction = "row",
+  justify = "flexBox-start",
+  align = "stretch",
+  wrap = "nowrap",
+  gap = 0,
+  grow,
+  shrink,
+  basis,
+  inline = false,
+  children,
+  className,
+  ...props
+}: FlexBoxProps) => {
+  const styles: React.CSSProperties = {
+    flexDirection: direction,
+    justifyContent: justify,
+    alignItems: align,
+    flexWrap: wrap,
+    gap: gap,
+    flexGrow: grow,
+    flexShrink: shrink,
+    flexBasis: basis,
+  };
+
+  return (
+    <div className={clsx(inline ? "inline-flex" : "flex", className)} style={styles} {...props}>
+      {children}
+    </div>
+  );
+};
