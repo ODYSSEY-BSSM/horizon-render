a67552b7c88f36d4f7d235c31f631e9277216f55
justn-hyeok <justn.hyeok@gmail.com>
2025-08-24 08:53:24 +0900

    build(#54): `apps/web` 프로젝트 세팅

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Horizon Render',
+  description: 'NextJS web application for Horizon Render design system',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='ko'>
+      <body>{children}</body>
+    </html>
+  );
+}
