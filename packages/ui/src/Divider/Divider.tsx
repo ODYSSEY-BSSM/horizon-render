3d0111b3c71900b7408e2d1f77c9c693ccea9232
sunz8281 <sunz8281@gmail.com>
2025-09-08 03:30:33 +0900

    feat(#85): Divider length 추가

--- packages/ui/src/Divider/Divider.tsx
+++ packages/ui/src/Divider/Divider.tsx
@@ -2,37 +2,48 @@ import styled from '@emotion/styled';
 import { tokens } from '@horizon/tokens';
 import type React from 'react';
 
-interface HorizontalProps extends React.HTMLAttributes<HTMLHRElement> {
+interface HorizontalProps extends Omit<React.HTMLAttributes<HTMLHRElement>, 'length'> {
   orientation?: 'horizontal';
+  length?: string;
 }
 
 interface VerticalProps extends React.HTMLAttributes<HTMLDivElement> {
   orientation: 'vertical';
+  length?: string;
 }
 
 export type DividerProps = HorizontalProps | VerticalProps;
 
-export const Divider = ({ orientation = 'horizontal', ...props }: DividerProps) => {
+export const Divider = ({
+  orientation = 'horizontal',
+  length = '100%',
+  ...props
+}: DividerProps) => {
   return orientation === 'vertical' ? (
-    <StyledVerticalDivider {...props} role='separator' aria-orientation='vertical' />
+    <StyledVerticalDivider
+      {...props}
+      role='separator'
+      aria-orientation='vertical'
+      {...{ length }}
+    />
   ) : (
-    <StyledHorizontalDivider {...props} />
+    <StyledHorizontalDivider {...props} {...{ length }} />
   );
 };
 
 Divider.displayName = 'Divider';
 
-const StyledHorizontalDivider = styled.hr`
-    margin: 0;
-    border: none;
-    width: 100%;
-    border-top: ${tokens.stroke.weight} solid ${tokens.stroke.color};
+interface StyledDividerProps {
+  length: string;
+}
+
+const StyledHorizontalDivider = styled.hr<StyledDividerProps>`
+  width: ${({ length }) => length};
+  border-top: ${tokens.stroke.weight} solid ${tokens.stroke.color};
 `;
 
-const StyledVerticalDivider = styled.div`
-  margin: 0;
-  border: none;
-  height: 100%;
+const StyledVerticalDivider = styled.div<StyledDividerProps>`
+  height: ${({ length }) => length};
   display: inline-block;
   border-left: ${tokens.stroke.weight} solid ${tokens.stroke.color};
 `;
