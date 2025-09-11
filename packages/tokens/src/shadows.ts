d299525bbb17dba0013d8d41bc50d43f4d101857
justn-hyeok <justn.hyeok@gmail.com>
2025-09-11 11:00:21 +0900

    feat(#78): 색상 토큰 추가 및 그림자 토큰 수정

--- packages/tokens/src/shadows.ts
+++ packages/tokens/src/shadows.ts
@@ -1,7 +1,3 @@
 export const shadows = {
-  0: '0px 0px 10px 0px rgba(0, 0, 0, 0.05)',
-  '+X': '4px 0px 10px 0px rgba(0, 0, 0, 0.05)',
-  '-X': '-4px 0px 10px 0px rgba(0, 0, 0, 0.05)',
-  '+Y': '0px 4px 10px 0px rgba(0, 0, 0, 0.05)',
-  '-Y': '0px -4px 10px 0px rgba(0, 0, 0, 0.05)',
+  0: '0px 1px 2px 0px rgba(225, 171, 171, 0.05)',
 } as const;
