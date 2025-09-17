81dbd102d2d2afc27aee887bb605c8c9a74f38ef
justn-hyeok <justn.hyeok@gmail.com>
2025-09-17 23:45:33 +0900

    feat(#110): ux 개선

--- apps/web/src/components/notfound/WinMessage/WinMessage.tsx
+++ apps/web/src/components/notfound/WinMessage/WinMessage.tsx
@@ -1,3 +1,4 @@
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 import { tokens } from '@horizon/tokens';
 
@@ -12,6 +13,17 @@ const WinMessage = () => {
   );
 };
 
+const slideInFromTop = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
 const StyledMainMessage = styled.p`
   font-family: 'SUIT Variable', sans-serif;
   font-size: 32px;
@@ -21,17 +33,10 @@ const StyledMainMessage = styled.p`
   margin-bottom: 40px;
   line-height: normal;
   text-align: center;
-  animation: slideInFromTop 0.8s ease-out;
+  animation: ${slideInFromTop} 0.8s ease-out;
 
-  @keyframes slideInFromTop {
-    0% {
-      opacity: 0;
-      transform: translateY(-30px);
-    }
-    100% {
-      opacity: 1;
-      transform: translateY(0);
-    }
+  @media (prefers-reduced-motion: reduce) {
+    animation: none;
   }
 `;
 
